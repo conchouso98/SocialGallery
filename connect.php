f233d9eac8164177c98846775d7775443266e81c
Miguel Angel Conchouso Cardoso <55998471+conchouso98@users.noreply.github.com>
2019-10-25 17:15:31 +0200

    Delete connect.php

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-        $servername = 'localhost';
-        $dbname = 'social_gallery';
-        $username = 'root';
-        $password = '';
-		$conn = new mysqli($servername, $username, $password, $dbname);
-?>
